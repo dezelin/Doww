45d1c9d4a4dbab672fc10b3fed9465a58d3d7918
Aleksandar Dezelin <dezelin@gmail.com>
2016-11-22 15:45:58 +0100

    dowwlib module renamed to doww

--- doww/src/androidTest/java/com/dezel/doww/ExampleInstrumentedTest.java
+++ doww/src/androidTest/java/com/dezel/doww/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.dezel.dowwlib;
+package com.dezel.doww;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.dezel.dowwlib.test", appContext.getPackageName());
+        assertEquals("com.dezel.doww.test", appContext.getPackageName());
     }
 }
